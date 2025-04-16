448a16b4ae5d558b090598e7136bd2bd66ebfd70
feelsuegood <suaj2021@gmail.com>
2025-04-16 20:18:26 +1000

    List, collection if

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
 void main() {
-  print("Hello, World!");
+  var giveMeFive = true;
+  var numbers = [
+    1, 2, 3,
+    // collection if
+    if (giveMeFive) 5,
+  ];
+  // List<int> numbers_sec = [1,2,3];
+  numbers.first;
+  // if (giveMeFive){
+  //   numbers.add(5);
+  // }
+  print(numbers);
 }
