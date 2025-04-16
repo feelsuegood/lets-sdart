9c01acf26955d96ee4adbd2cba9088819da7905c
feelsuegood <suaj2021@gmail.com>
2025-04-16 20:41:21 +1000

    map and set

--- main.dart
+++ main.dart
@@ -1,10 +1,22 @@
 void main() {
-  var oldFriends = ["sue", "john"];
-  var newFriends = [
-    "lewis",
-    "ralph",
-    "darren",
-    for (var friend in oldFriends) "😎 $friend",
-  ];
-  print(newFriends);
+  Map<List<int>, bool> player = {
+    [1, 2, 3]: true,
+    [5, 3]: false,
+    [1, 1]: false,
+  };
+  var player2 = {
+    [1, 2, 3]: true,
+    [5, 3]: false,
+    [1, 1]: false,
+  };
+
+  print(player.isNotEmpty);
+
+  // every elements in sets are unique
+  var numbers = {1, 2, 3};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
 }
