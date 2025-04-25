8529a94973b60e7a39b18e0b5c1bb8efd46183c4
feelsuegood <suaj2021@gmail.com>
2025-04-25 22:14:09 +1000

    named parameters

--- main.dart
+++ main.dart
@@ -1,22 +1,37 @@
+// void sayHello(String neme){
+//   print("Hello $name, Nice to meet you")
+// }
+
+// String sayHello(String name) {
+//   return "Hello $name, nice to meet you";
+// }
+
+// String sayHello(String name) => "Hello $name, nice to meet you";
+
+// * num accpets int, double
+// num plus(num a, num b) => a + b;
+
+// * positional parameters
+// String sayHello(String name, int age, String country) {
+//   return "Hello $name, you are $age and come from $country";
+// }
+
+// * named parameters, order doesn't matter
+// * Set default values
+// String sayHello({String name = "who", int age = 99, String country = "where"}) {
+//   return "Hello $name, you are $age and come from $country";
+// }
+
+// using required modifier
+String sayHello({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return "Hello $name, you are $age and come from $country";
+}
+
 void main() {
-  Map<List<int>, bool> player = {
-    [1, 2, 3]: true,
-    [5, 3]: false,
-    [1, 1]: false,
-  };
-  var player2 = {
-    [1, 2, 3]: true,
-    [5, 3]: false,
-    [1, 1]: false,
-  };
-
-  print(player.isNotEmpty);
-
-  // every elements in sets are unique
-  var numbers = {1, 2, 3};
-  numbers.add(1);
-  numbers.add(1);
-  numbers.add(1);
-  numbers.add(1);
-  print(numbers);
+  print(sayHello(country: "USA", name: "Sukuna", age: 11));
+  // print(plus(1, 4));
 }
