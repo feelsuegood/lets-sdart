5c51203db31365346dd26bf61d9a034e6c4d896b
feelsuegood <suaj2021@gmail.com>
2025-04-25 22:57:02 +1000

    constructors

--- main.dart
+++ main.dart
@@ -1,22 +1,25 @@
-// in Class, should set type of property
 class Player {
-  final String name = 'Sue';
-  int xp = 1999;
+  // late final String name;
+  // late int xp;
+  // * constructor method - should be same name with class name
+  // Player(String name, int xp) {
+  //   this.name = name;
+  //   this.xp = xp;
+  // }
+
+  final String name;
+  int xp;
+
+  Player(this.name, this.xp);
 
   void sayHello() {
-    // no need to use $this.name
     print("Hello $name, nice to meet you");
-    // * in case below, use ${this.name}
-    // var name ='haha';
-    // print("Hello ${this.name}, nice to meet you");
   }
 }
 
 void main() {
-  // var player = new Player(); // new is not required
-  var player = Player();
-  // print(player.name);
-  // player.name = "sukuna";
-  // print(player.name);
+  var player = Player("Sue", 9800);
   player.sayHello();
+  var player2 = Player("Jacob", 9600);
+  player2.sayHello();
 }
