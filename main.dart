e22b6df2bee10a46d854d7fc95b907791a8b2cee
feelsuegood <suaj2021@gmail.com>
2025-04-26 23:34:54 +1000

    4.3 named constructor

--- main.dart
+++ main.dart
@@ -1,9 +1,10 @@
 class Player {
   final String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
+  // int age;
 
+  // * named constuctor
   Player({
     required this.name,
     required this.xp,
@@ -11,14 +12,27 @@ class Player {
     required this.age,
   });
 
+  Player.createBluePlayer({required String name, required int age})
+    : this.name = name,
+      this.age = age,
+      this.team = 'blue',
+      this.xp = 0;
+
+  // by default, all positional parameters are required
+  Player.createRedPlayer(String name, int age)
+    : this.name = name,
+      this.age = age,
+      this.team = 'red',
+      this.xp = 0;
+
   void sayHello() {
     print("Hello $name, nice to meet you");
   }
 }
 
 void main() {
-  var player = Player(name: "Sue", xp: 9800, team: "Purple", age: 89);
+  var player = Player.createRedPlayer("Sue", 89);
   player.sayHello();
-  var player2 = Player(name: "Jacob", xp: 9999, team: "Red", age: 33);
+  var player2 = Player.createBluePlayer(name: "Jacob", age: 33);
   player2.sayHello();
 }
