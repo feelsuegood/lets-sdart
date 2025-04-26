bb244d5c90b769dd74e5ea02c5b481613d836db9
feelsuegood <suaj2021@gmail.com>
2025-04-27 07:25:22 +1000

    abstract class

--- main.dart
+++ main.dart
@@ -1,17 +1,33 @@
-// * enum constraints choices
+// * abstract class can't have objfects -> provide blue prints
+// specify methods of classes that extends an abstract class
+
+abstract class Human {
+  void walk();
+}
+
 enum Team { pink, purple }
 
 enum XPLevel { biginner, intermediate, professional }
 
-class Player {
+class Player extends Human {
   String name;
   XPLevel xp;
   Team team;
 
   Player({required this.name, required this.xp, required this.team});
 
+  void walk() {
+    print("I'm walking");
+  }
+
   void sayHello() {
     print("Hello $name, nice to meet you");
+  }
+}
+
+class Coach extends Human {
+  void walk() {
+    print("A coach is walking");
   }
 }
 
